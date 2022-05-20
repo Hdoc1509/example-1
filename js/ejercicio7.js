db2dbef8dc09c21f831a9a635715aa63f0fd806f
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-19 19:03:25 -0500

    Refactor: Refactorized js/ejercicio7.js

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,23 +1,20 @@
+import { gid } from './utils/dom.js';
+
 /*
 Ejercicio 7: Construir un programa que, dado un número total de horas, devuelve
 el número de semanas, días y horas equivalentes. Por ejemplo, dado un total de
 1000 horas debe mostrar 5 semanas, 6 días y 16 horas.
 */
 
-document.getElementById("calculo7").addEventListener("click",function(){
-	let totalHoras = parseInt(document.getElementById("totalHoras").value);
+gid('ej7').addEventListener('submit', (e) => {
+	e.preventDefault();
 
-	let semanas = parseInt(totalHoras/168);
-	let dias = parseInt((totalHoras%168)/24);
-	let horas = totalHoras%24;
+	const totalHoras = Number(e.target.totalHoras.value);
+	const semanas = parseInt(totalHoras / 168);
+	const dias = parseInt((totalHoras % 168) / 24);
+	const restoHoras = parseInt(totalHoras % 24);
 
-	if(isNaN(totalHoras)){
-		semanas = "";
-		dias = "";
-		horas = "";
-	}
-
-	document.getElementById("semanas").value = semanas;
-	document.getElementById("dias").value = dias;
-	document.getElementById("restoHoras").value = horas;
+	e.target.semanas.value = semanas;
+	e.target.dias.value = dias;
+	e.target.restoHoras.value = restoHoras;
 });
