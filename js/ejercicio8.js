b59f8e64bb5c69d0f8b843e4a91c0a5eaa197bea
Héctor Ochoa <blueguitarist1520@gmail.com>
2022-05-19 19:16:31 -0500

    Refactorized js/ejercicio8.js

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,3 +1,6 @@
+import { gid } from './utils/dom.js';
+import { formInputs } from './utils/form.js';
+
 /*
 Ejercicio 8: Construir un programa que calcule y muestre por pantalla las raíces
 de la ecuación de segundo grado de coeficientes reales.
@@ -6,19 +9,23 @@ x1 = (-b + Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
 x2 = (-b - Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a)
 */
 
-document.getElementById("calculo8").addEventListener("click",function(){
-	let a = parseInt(document.getElementById("coeA").value);
-	let b = parseInt(document.getElementById("coeB").value);
-	let c = parseInt(document.getElementById("coeC").value);
+gid('ej8').addEventListener('submit', (e) => {
+	e.preventDefault();
 
-	let x1 = (-b+Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
-	let x2 = (-b-Math.sqrt(Math.pow(b,2)-4*a*c))/(2*a);
+	const { coeA: a, coeB: b, coeC: c } = formInputs(e.target);
+	const { raiz1, raiz2 } = e.target;
 
-	if(isNaN(x1)||isNaN(x2)){
-		x1 = "No hay raíz";
-		x2 = "No hay raíz";
+	const discriminant = Number(b) ** 2 - 4 * Number(a) * Number(c);
+
+	if (discriminant < 0) {
+		raiz1.value = 'No hay raíz';
+		raiz2.value = 'No hay raíz';
+		return;
 	}
 
-	document.getElementById("raiz1").value = x1;
-	document.getElementById("raiz2").value = x2;
+	const x1 = (-Number(b) + Math.sqrt(discriminant)) / (2 * Number(a));
+	const x2 = (-Number(b) - Math.sqrt(discriminant)) / (2 * Number(a));
+
+	raiz1.value = x1;
+	raiz2.value = x2;
 });
